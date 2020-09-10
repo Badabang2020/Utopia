d4d6f35bf14904856849bffdcdeecf3e4e3c6a5e
Raphael <raphael.m.resl@gmail.com>
2020-09-10 14:30:07 +0200

    changed Citizen, HealthStatus set to CitizenStatus

---
+++ src/CitizenStatus.java
@@ -0,0 +1,65 @@
+public class CitizenStatus {
+    private final int MINCITIZENSTATUSUP = 0;
+    private final int MAXCITIZENSTATUSUP = 100;
+    private final int MAXCITIZENSTATUSDOWN = 100;
+    private final int MINCITIZENSTATUSDOWN = 0;
+
+    public class MainStatus {
+        private int wallet = 0;
+        private int healthbar = 100;
+        private String event = "free";
+        private int eventTime = 0;
+
+//        MainStatus (int wallet, int healthbar){
+//            this.wallet = wallet;
+//            this.healthbar = healthbar;
+//            this.event = "free";
+//            this.eventTime = 0;
+//        }
+    }
+
+    public class Emotions {
+        private int happiness;
+        private int love;
+        private int fear;
+        private int sadness;
+        private int anger;
+
+//        Emotions (int happiness, int love, int fear, int sadness, int anger) {
+//            this.happiness = happiness;
+//            this.love = love;
+//            this.fear = fear;
+//            this.sadness = sadness;
+//            this.anger = anger;
+//        }
+
+        private void generateEmotions () {
+            this.happiness = (int)(Math.random()*100);
+            this.love = (int)(Math.random()*100);
+            this.fear = (int)(Math.random()*100);
+            this.sadness = (int)(Math.random()*100);
+            this.anger = (int)(Math.random()*100);
+        }
+    }
+
+    public class Needs {
+        private int sleep;
+        private int hunger;
+        private int thirst;
+        private int toilet;
+
+        Needs(int sleep, int hunger, int thirst, int toilet){
+            this.sleep = sleep;
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.toilet = toilet;
+        }
+    }
+
+
+
+    public int setRandomStats (){
+
+        return 10;
+    }
+}
