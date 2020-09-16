22c69004448364e938a019330b568f88e97ec106
Noran <matschiff@gmx.de>
2020-09-16 13:03:55 +0200

    ambulance fix

--- src/Ambulance.java
+++ src/Ambulance.java
@@ -1,20 +1,11 @@
 public class Ambulance implements Event{
 
     public void happens(Citizen citizen){
-<<<<<<< HEAD
         int health = citizen.getCitizenStatus().getMainStatus().getHealthbar();
 
         if (health < 10) {
             health = health + 25;
             citizen.getCitizenStatus().getMainStatus().setHealthbar(health);
-=======
-        int health = 0;
-
-        health = citizen.getCitizenStatus().getMainStatus().getHealthbar();
-        if (health < 50) {
-            health = health + 50;
-            citizen.getCitizenStatus().getMainStatus().setHealthbar() = health;
->>>>>>> Ambulance
         }
     };
     public void tick(){
