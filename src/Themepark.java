88055d84c6d5cc59cf4f2ceb7a11e4a86dd4f7bf
Ben Gombl <gomboc95@gmx.at>
2020-09-16 16:11:54 +0200

    themepark rollercoaster

--- src/Themepark.java
+++ src/Themepark.java
@@ -5,6 +5,8 @@ public class Themepark implements Event {
     private ArrayList<Citizen> guests = new ArrayList<Citizen>();
     private Rollercoaster rollercoaster = new Rollercoaster();
 
+    private String msg = "|at themepark|";
+
     public class Rollercoaster implements Event {
 
         ArrayList queue = new ArrayList();
@@ -13,23 +15,25 @@ public class Themepark implements Event {
         public void happens(Citizen citizen) {
             queue.add(citizen);
             citizen.getCitizenStatus().getMainStatus().setEventTime(-1);
-            citizen.getCitizenStatus().getMainStatus().setEvent("goes into queue for Rollercoaster");
+            citizen.getCitizenStatus().getMainStatus().setEvent(msg + "goes into queue for Rollercoaster");
         }
         @Override
         public void tick(){
             for (int i = 0; i < queue.size(); i++) {
                 Citizen guest = (Citizen) queue.get(i);
-                guest.getCitizenStatus().getMainStatus().setEvent("waits for Rollercoaster");
+                guest.getCitizenStatus().getMainStatus().setEvent(msg + "waits for Rollercoaster");
             }
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 1; i++) {
                 if(queue.size()>0){
 
-                    Citizen guest = (Citizen) queue.get(queue.size()-1);
-                    queue.remove(queue.size()-1);
+                    Citizen guest = (Citizen) queue.get(0);
+                    queue.remove(0);
                     guest.getCitizenStatus().getMainStatus().setEventTime(1);
                     guest.getCitizenStatus().getMainStatus().setWallet(guest.getCitizenStatus().getMainStatus().getWallet()-10);//guest.money -= 10;
 
-                    guest.getCitizenStatus().getMainStatus().setEvent("takes a Ride in Rollercoaster");
+                    guest.getCitizenStatus().getMainStatus().setEvent(msg + "takes a Ride in Rollercoaster");
+
+                    guests.add(guest);
                 }
 
             }
@@ -53,17 +57,20 @@ public class Themepark implements Event {
     @Override
     public void tick() {
         Citizen current;
-        for (int i = 0; i < guests.size(); i++) {
-            current = guests.get(i);
+        for (int i = 0; i < guests.size();i = 0) {
+            current = guests.get(0);
             if (current.getCitizenStatus().getMainStatus().getWallet() <= 10) {
                 current.getCitizenStatus().getMainStatus().setEventTime(0);
                 current.getCitizenStatus().getMainStatus().setEvent("leaves the Themepark");
-                guests.remove(i);
+                guests.remove(0);
 
             } else {
                 current.doEvent(rollercoaster);
+                guests.remove(0);
             }
         }
+
+    rollercoaster.tick();
     }
 
 }
--- src/UtopiaMain.java
+++ src/UtopiaMain.java
@@ -1,9 +1,17 @@
 import java.text.ParseException;
 
+
+
 public class UtopiaMain {
     static RecordSection utopiaCitizenRecords;
     static GKK employeeCare;
     static SVA companyCare;
+
+    public static String eventtrack(Citizen citizen){
+        return "" + citizen.getFirstName() + "   " + citizen.getCitizenStatus().getMainStatus().getEvent();
+    }
+
+
 
     public static void main(String[] args) throws ParseException {
         utopiaCitizenRecords = new RecordSection();
@@ -11,18 +19,116 @@ public class UtopiaMain {
         companyCare = new SVA();
         Bank bank = new Bank();
         Restaurant restaurant = new Restaurant();
+        Themepark themepark = new Themepark();
 
 
-        Citizen citizen = new Citizen("Moritz", "Mustermann", "08/15", 'm', 65, new Address(), employeeCare, false, new CitizenStatus());
+        Citizen citizen = new Citizen("Moritz", "Mustermann", "08/12", 'm', 65, new Address(), employeeCare, false, new CitizenStatus());
+        Citizen citizen1 = new Citizen("fabian", "Mustermann", "08/333", 'm', 65, new Address(), employeeCare, false, new CitizenStatus());
+        Citizen citizen2 = new Citizen("ben", "Mustermann", "08/123235", 'm', 65, new Address(), employeeCare, false, new CitizenStatus());
+        Citizen citizen3 = new Citizen("mathias", "Mustermann", "084254/15", 'm', 65, new Address(), employeeCare, false, new CitizenStatus());
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
 
-        System.out.println(citizen);
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
 
         citizen.doEvent(bank);
+        citizen1.doEvent(bank);
+        citizen2.doEvent(bank);
+        citizen3.doEvent(bank);
 
-        System.out.println(citizen);
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
 
-        citizen.doEvent(restaurant);
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
 
-        System.out.println(citizen);
+        citizen.doEvent(themepark);
+        citizen1.doEvent(themepark);
+        citizen2.doEvent(themepark);
+        citizen3.doEvent(themepark);
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
+
+        themepark.tick();
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
+
+        themepark.tick();
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
+
+        themepark.tick();
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
+
+        themepark.tick();
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
+
+        themepark.tick();
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
+
+        themepark.tick();
+
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("");
+
+        System.out.println(eventtrack(citizen));
+        System.out.println(eventtrack(citizen1));
+        System.out.println(eventtrack(citizen2));
+        System.out.println(eventtrack(citizen3));
+
+
     }
 }
