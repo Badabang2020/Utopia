b02f6e2dfb95664d481e6651f686322bb85e886a
Bruckii <michaelbruckbauer@gmx.at>
2020-09-16 12:52:35 +0200

    Restaurant

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 
-public class Restaurant extends Facilities implements Event {
+public class Restaurant implements Event {
     ArrayList<MenuItem> menuItems;
 
     Restaurant() {
         menuItems = new ArrayList<MenuItem>();
-        menuItems.add(new MenuItem("Small Menu", 3, -3, -10, 3));
-        menuItems.add(new MenuItem("Large Beer", 15, -4, -20, 10));
-        menuItems.add(new MenuItem("Medium Menu", 5, -5, -15, 5));
-        menuItems.add(new MenuItem("Super Large Menu", 10, -10, -30, 10));
+        menuItems.add(new MenuItem("Large Menu", 10, 50, -30, 10));
+        menuItems.add(new MenuItem("Medium Menu", 5, 30 , -15, 5));
+        menuItems.add(new MenuItem("Small Menu", 3, 10, -10, 3));
     }
 
     private class MenuItem {
@@ -24,28 +23,21 @@ public class Restaurant extends Facilities implements Event {
             this.toilet = toilet;
             this.hunger = hunger;
         }
-        Double getRate(Citizen citizen, ArrayList<String> attributes) {
-            int citizenHunger = citizen.getCitizenStatus().getNeeds().getHunger();
-            int tmpHunger = citizenHunger - hunger;
-            return 10.0 - tmpHunger; // 10 = best rating
-        }
-
     }
-    // 3 Menüs: klein mittel groß
+    @Override
     public void happens(Citizen citizen) {
-
+        int citizenHunger = citizen.getCitizenStatus().getNeeds().getHunger();
+        int citzenMoney = citizen.getCitizenStatus().getMainStatus().getWallet();
+        for (MenuItem m: menuItems) {
+            if (100 - citizenHunger > m.hunger && citzenMoney > m.cost) {
+                citizen.getCitizenStatus().getNeeds().setHunger(citizenHunger + m.hunger);
+                citizen.getCitizenStatus().getMainStatus().setWallet(citzenMoney - m.cost);
+            }
+        }
     }
 
     @Override
-    void getBestService(Citizen citizen, ArrayList<String> attributes ) {
-        MenuItem bestItem = null;
-        Double rating = -100.0;
-        for (MenuItem m: menuItems) {
-            if (m.getRate(citizen,attributes) > rating) {
-                rating = m.getRate(citizen,attributes);
-                bestItem = m;
-            }
-        }
-        return citizen.doEvent();
+    public void tick() {
+
     }
 }
