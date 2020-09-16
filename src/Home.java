41884749ac0061c1368a3402217a451b1e96dcba
Raphael <raphael.m.resl@gmail.com>
2020-09-16 13:31:35 +0200

    ready for test

--- src/Home.java
+++ src/Home.java
@@ -16,4 +16,20 @@ public class Home {
             citizen.getCitizenStatus().getMainStatus().setEventTime(1);
         }
     }
+
+    public void goToToilet(Citizen citizen){
+        int checkToilet = citizen.getCitizenStatus().getNeeds().getToilet();
+
+        if(checkToilet < 15) {
+            citizen.getCitizenStatus().getNeeds().setToilet(100);
+            citizen.getCitizenStatus().getMainStatus().setEvent("Is on a big shit");
+            citizen.getCitizenStatus().getMainStatus().setEventTime(1);
+        }
+        else if(checkToilet < 50){
+            checkToilet += 15;
+            citizen.getCitizenStatus().getNeeds().setToilet(checkToilet);
+            citizen.getCitizenStatus().getMainStatus().setEvent("Is on a small business");
+            citizen.getCitizenStatus().getMainStatus().setEventTime(1);
+        }
+    }
 }
