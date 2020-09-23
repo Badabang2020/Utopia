716c78b6124d4f3d068952e3a3723b18fd9afeb3
Jennifer <jennifer.zarazik.93@gmail.com>
2020-09-23 14:56:10 +0200

    jobs update

--- src/Jobs.java
+++ src/Jobs.java
@@ -1,2 +1,44 @@
-public class Jobs {
+import java.util.ArrayList;
+
+public class Jobs implements Event {
+    ArrayList<JobsCategory> joblist;
+
+    Jobs(){
+        joblist = new ArrayList<>();
+        joblist.add((new JobsCategory("Zeitungszusteller" ,75,2, "work as a Zeitungszusteller and gain 75 Utopia Dollars.")));
+        joblist.add((new JobsCategory("Aushilfskraft",150,4, "work as a Aushilfskraft and gain 150 Utopia Dollars.")));
+        joblist.add((new JobsCategory("Vollzeit", 300,8, "work as a Vollzeit and gain 150 Utopia Dollars.")));
+
+    }
+
+    private class JobsCategory{
+        public String jobname;
+        public int wallet;
+        public int duration;
+        public String text;
+
+    JobsCategory(String jobname, int wallet, int duration, String text ){
+        this.jobname = jobname;
+        this.wallet = wallet;
+        this.duration = duration;
+        this.text = text;
+    }
+    }
+
+    @Override
+    public void happens(Citizen citizen){
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+
+    @Override
+    public Category[] getCategory() {
+        return new Category[] {Category.Money};
+    }
+
 }
