cb9caa1f62dce5bcc406b3cb444734afdd517316
Ben Gombl <gomboc95@gmx.at>
2020-09-24 17:20:44 +0200

    created  Event Death and Event Graveyard

---
+++ src/Facilities/deathAndFuneral/Graveyard.java
@@ -0,0 +1,69 @@
+package Facilities.deathAndFuneral;
+
+
+import Citizen.Citizen;
+import Facilities.Facilities;
+import UtopiaCore.Category;
+import UtopiaCore.Event;
+import UtopiaCore.GlobalStacker;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Graveyard implements Event {
+
+    ArrayList mourners = new ArrayList<Citizen>();//An Arraylist of mourning Citizens
+    ArrayList buried = new ArrayList<Citizen>();//An Arraylist of dead Citizens buried in this graveyard
+    int ticksSinceLastBurial = 1000;
+    String msg = "at the graveyard";//default event-msg for alive citizens who are at the graveyard
+
+    @Override
+    public void happens(Citizen citizen) {
+        mourners.add(citizen);
+        citizen.getCitizenStatus().getMainStatus().setEvent("goes to graveyard");
+        citizen.getCitizenStatus().getEmotions().setHappiness((citizen.getCitizenStatus().getEmotions().getHappiness() - 5));
+        citizen.getCitizenStatus().getMainStatus().setEventTime(4);
+    }
+
+    @Override
+    public void tick() {
+        ticksSinceLastBurial++;
+        for (int i = 0; i < buried.size(); i++) {
+            Citizen current = (Citizen) buried.get(i);
+            current.getCitizenStatus().getMainStatus().setEvent("dead");
+        }
+        //funerals can occur if there are dead citizens who haven't had a funeral and enough time since the last funeral has passed.
+        //funerals affect all citizens who are currently at the graveyard
+        if (Death.notBuried.size() > 0 && ticksSinceLastBurial > 7) {
+            ticksSinceLastBurial = 0;
+            Citizen corpse = (Citizen) Death.notBuried.get(0);//takes the next Citizen from Death.notBuried
+            corpse.getCitizenStatus().getMainStatus().setEvent("getting buried at the Graveyard");
+            Death.notBuried.remove(0);//removes the buried Citizen from Death.notBuried list
+            this.buried.add(corpse);
+            while (mourners.size() > 0) {
+                Citizen current = (Citizen) mourners.get(0);
+                current.getCitizenStatus().getMainStatus().setEvent(msg + "|| mourning the death of" + corpse.getFirstName() + " " + corpse.getLastName());
+                mourners.remove(0);
+                current.getCitizenStatus().getMainStatus().setEventTime(4);
+                current.getCitizenStatus().getEmotions().setSadness(current.getCitizenStatus().getEmotions().getSadness() - 30);
+                current.getCitizenStatus().getEmotions().setHappiness((current.getCitizenStatus().getEmotions().getHappiness() - 30));
+            }
+        } else {
+            for (int i = 0; i < mourners.size(); i++) {
+                Citizen current = (Citizen) mourners.get(i);
+                current.getCitizenStatus().getMainStatus().setEvent(msg);
+                if (current.getCitizenStatus().getMainStatus().getEventTime() == 0) {
+                    mourners.remove(i);
+                    i--;
+                }
+            }
+
+        }
+
+    }
+
+    @Override
+    public Category[] getCategory() {
+        return new Category[]{};
+    }
+}
--- src/UtopiaCore/Tester.java
+++ src/UtopiaCore/Tester.java
@@ -2,6 +2,8 @@ package UtopiaCore;
 
 import Bank.Bank;
 import Facilities.*;
+import Facilities.deathAndFuneral.Death;
+import Facilities.deathAndFuneral.Graveyard;
 import Health.*;
 import Citizen.*;
 import HealthInsurance.*;
@@ -20,6 +22,9 @@ public class Tester {
     Event ambulance = new Ambulance();
     Event cinema = new Cinema();
     Event hospital = new Hospital();
+    Event death = new Death();
+    Event graveyard = new Graveyard();
+
 
     Random rand = new Random();
 
@@ -43,10 +48,13 @@ public class Tester {
             UtopiaMain.myController.registerActivity(ambulance);
             UtopiaMain.myController.registerActivity(cinema);
             UtopiaMain.myController.registerActivity(hospital);
+            UtopiaMain.myController.registerActivity(graveyard);
+            UtopiaMain.myController.registerActivity(death);
 
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < 5; i++) {
                 UtopiaMain.myController.registerCitizen(new Citizen("" + i, ""+i+"!", "" + rdm.nextInt(1000000000), 'm', rdm.nextInt(100), new Address(), new GKK(), false, new CitizenStatus()));
-            }        GlobalStacker.registredCitizens.get(0).getCitizenStatus().getMainStatus().setHealthbar(5);
+            }        GlobalStacker.registredCitizens.get(0).getCitizenStatus().getMainStatus().setHealthbar(-5);
+            GlobalStacker.registredCitizens.get(1).getCitizenStatus().getMainStatus().setHealthbar(-5);
 
         }
 
@@ -55,8 +63,10 @@ public class Tester {
         for (int i = 0 ; i < GlobalStacker.registredCitizens.size(); i++) {
             Citizen citizen = GlobalStacker.registredCitizens.get(i);
             if(citizen.getCitizenStatus().getMainStatus().getEventTime()==0){
-                if(citizen.getCitizenStatus().getMainStatus().getWallet()<30 && citizen.getCitizenStatus().getMainStatus().getEventTime() == 0){
-                    citizen.doEvent(ambulance);
+                if (citizen.getCitizenStatus().getMainStatus().getHealthbar()<0){
+                    citizen.doEvent(death);
+                }else{
+                    citizen.doEvent(graveyard);
                 }
 
             }
