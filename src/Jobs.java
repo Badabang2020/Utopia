95b4f2200d7dd4766ba6cbd9785d6e7801768b6a
Jennifer <jennifer.zarazik.93@gmail.com>
2020-09-24 11:58:28 +0200

    work on jobs

--- src/Jobs.java
+++ src/Jobs.java
@@ -5,9 +5,9 @@ public class Jobs implements Event {
 
     Jobs(){
         joblist = new ArrayList<>();
-        joblist.add((new JobsCategory("Zeitungszusteller" ,75,2, "work as a Zeitungszusteller and gain 75 Utopia Dollars.")));
-        joblist.add((new JobsCategory("Aushilfskraft",150,4, "work as a Aushilfskraft and gain 150 Utopia Dollars.")));
-        joblist.add((new JobsCategory("Vollzeit", 300,8, "work as a Vollzeit and gain 150 Utopia Dollars.")));
+        joblist.add((new JobsCategory("Zeitungszusteller" ,75,2, -5,-5,"work as a Zeitungszusteller and gain 75 Utopia Dollars.")));
+        joblist.add((new JobsCategory("Aushilfskraft",150,4, -10,-10,"work as a Aushilfskraft and gain 150 Utopia Dollars.")));
+        joblist.add((new JobsCategory("Vollzeit", 300,8, -20,-20,"work as a Vollzeit and gain 150 Utopia Dollars.")));
 
     }
 
@@ -15,18 +15,30 @@ public class Jobs implements Event {
         public String jobname;
         public int wallet;
         public int duration;
+        int happyness;
+        int sleep;
         public String text;
 
-    JobsCategory(String jobname, int wallet, int duration, String text ){
+    JobsCategory(String jobname, int wallet, int duration, int happyness, int sleep, String text ){
         this.jobname = jobname;
         this.wallet = wallet;
         this.duration = duration;
+        this.happyness= happyness;
+        this.sleep = sleep;
         this.text = text;
     }
     }
+    
 
     @Override
     public void happens(Citizen citizen){
+        int citizenHappyness = citizen.getCitizenStatus().getEmotions().getHappiness();
+        int citizenSpleep = citizen.getCitizenStatus().getNeeds().getSleep();
+        int citizenWallet = citizen.getCitizenStatus().getMainStatus().getWallet();
+
+        citizen.getCitizenStatus().getEmotions().setHappiness(citizenHappyness);
+        citizen.getCitizenStatus().getNeeds().setSleep(citizenSpleep);
+        citizen.getCitizenStatus().getMainStatus().setWallet(citizenWallet);
 
     }
 
